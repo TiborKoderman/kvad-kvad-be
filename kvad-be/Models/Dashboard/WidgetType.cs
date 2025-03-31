72158bc930bf86bc2374d3c6c3077bf9736752fe
Tibor Koderman <tibor.koderman@inea.si>
2025-03-31 09:05:14 +0200

    Remove unused properties from WidgetType model for cleaner code

--- kvad-be/Models/Dashboard/WidgetType.cs
+++ kvad-be/Models/Dashboard/WidgetType.cs
@@ -6,7 +6,5 @@ public class WidgetType
     public required string Name { get; set; }
     public required string Description { get; set; }
     public required string VueComponent { get; set; }
-    // public required JsonNode Config { get; set; } = new JsonObject();
-    // public required JsonNode Params { get; set; } = new JsonObject();
 
 }
