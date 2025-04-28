f3beec5255ca170b84c580933f3a053b99d32c07
Tibor Koderman <tibor.koderman@inea.si>
2025-04-28 14:36:19 +0200

    Add Source property to Tag model for better data categorization

--- kvad-be/Models/Tag/Tag.cs
+++ kvad-be/Models/Tag/Tag.cs
@@ -8,6 +8,7 @@ public class Tag
     public required Device Device { get; set; }
     public required string Id { get; set; }
     public required Unit Unit { get; set; }
+    public required string Source { get; set; } = ""; //Virtual, mqtt, computed, modbus
     public required bool Virtual { get; set; } = false;
     public required bool Enabled { get; set; } = true;
     public required bool Historicize { get; set; } = false;
