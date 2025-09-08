143a8c2501c2b3ce2b590fc5377fa5e6a77f084d
tibor <tibor.koderman@gmail.com>
2025-09-09 01:12:32 +0200

    feat(models): Update TagHistAgg to use Bucket instead of Timestamp for improved data aggregation

--- kvad-be/Models/Device/Tag/TagHistAgg.cs
+++ kvad-be/Models/Device/Tag/TagHistAgg.cs
@@ -1,12 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using NodaTime;
 
-[PrimaryKey(nameof(TagId), nameof(Timestamp))]
-public class TagHistAgg {
+[PrimaryKey(nameof(TagId), nameof(Bucket))]
+public class TagHistAgg
+{
   public required int TagId { get; set; }
-  public required Instant Timestamp { get; set; }
-  public double Min { get; set; }
-  public double Max { get; set; }
-  public double Sum { get; set; }
+  public required Instant Bucket { get; set; }
+  public double? Min { get; set; }
+  public double? Max { get; set; }
+  public double? Sum { get; set; }
   public int Count { get; set; }
 }
