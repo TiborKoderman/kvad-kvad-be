6ff33ac7ebd3bd2dc496d71fbed9a26a9d832a12
Tibor Koderman <tibor.koderman@gmail.com>
2025-11-04 19:53:02 +0100

    a

--- kvad-be/20251104135124_UpdateTo9_0_10.Designer.cs
+++ kvad-be/20251104135124_UpdateTo9_0_10.Designer.cs
@@ -6,6 +6,7 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NodaTime;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -13,14 +14,13 @@ using NpgsqlTypes;
 using kvad_be.Database;
 
 #nullable disable
-
-namespace kvad_be.Migrations
+[DbContext(typeof(AppDbContext))]
+[Migration("20251104135124_UpdateTo9_0_10")]
+partial class UpdateTo9_0_10
 {
-    [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    /// <inheritdoc />
+    protected override void BuildTargetModel(ModelBuilder modelBuilder)
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
-        {
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("ProductVersion", "9.0.10")
@@ -1686,4 +1686,3 @@ namespace kvad_be.Migrations
 #pragma warning restore 612, 618
         }
     }
-}
